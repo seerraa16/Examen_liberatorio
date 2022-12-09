4e7556f2c50d22509b758a8aee87989304d799c2
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-09 10:45:22 +0100

    fibonacci

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,10 +1,8 @@
-def cipher(clearText, key)
-    result = ""
-    matrix = [["" for x in range(len(clearText))] for y in range(key)]
-    return result
-
-def cipher(ciphertext, key):
-def transpose( m )
-    result = ""
-    matrix = [["" for x in range(len(clearText))] for y in range(key)]
-    return result
+def main():
+    print("jugar")
+    print()
+    cleartext = "defend the east wall xx"
+    print("original Text: " + cleartext)
+    print()
+    key = 3
+    
--- patron.py
+++ patron.py
@@ -3,3 +3,14 @@ patron = [
     ["D", "E", "F"],
     ["G", "H", "I"],
 ]
+for i in range (9):
+    print(patron)
+    def cipher(clearText, key):
+        result = ""
+        matrix = [["" for x in range(len(clearText))] for y in range(key)]
+        return result
+    def cipher(ciphertext, key):
+        def transpose( m ):
+        result = ""
+        matrix = [["" for x in range(len(clearText))] for y in range(key)]
+        return result
